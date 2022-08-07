091970ce708fa0f08cf66972e10920b3822883e4
累了 <100540507+yycx-bjyx@users.noreply.github.com>
2022-08-07 13:03:06 +1200

    yes_no_checker

---
+++ 01_learn_jp_before_or_not_v3.py
@@ -0,0 +1,20 @@
+# Functions
+def yes_no(question):
+    valid = False
+    while not valid:
+        # by making a variable for the t / f statement, it is not a lazy coding...
+        response = input(question).lower()
+        # .lower() at the end of statement input indicates that the variable is being put as a lower case, and is not case-sensitive
+        if response == "yes" or response == "y":
+            response = "yes"
+        elif response == "no" or response == "n":
+            response = "no"
+        else:
+            print("please answer yes / no")
+# Main Routine...
+
+
+instructions = yes_no("Have you learnt Japanese before? ")
+
+print("you chose {}".format(instructions))
+print()
