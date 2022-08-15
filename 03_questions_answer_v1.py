01daf658fe09ad106bf3008ba1d35418bea420bf
累了 <100540507+yycx-bjyx@users.noreply.github.com>
2022-08-16 09:37:49 +1200

    Update 03_questions_answer_v1.py

--- 03_questions_answer_v1.py
+++ 03_questions_answer_v1.py
@@ -5,5 +5,5 @@ how_much= 10
 questions = ["ら", "は", "さ", "ち", "ほ", "め", "ん", "ま", "む","ぬ"]
 
 for item in range(0, how_much):
-    ran_questions = random.choice(questions)
+    ran_questions = random.sample(questions)
     print(ran_questions)
