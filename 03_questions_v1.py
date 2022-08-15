b7d5ebb4e0ecf047eb4fffc8dc6b82880a9373b4
累了 <100540507+yycx-bjyx@users.noreply.github.com>
2022-08-16 09:40:25 +1200

    Delete 03_questions_v1.py

--- 03_questions_v1.py
+++
@@ -1,9 +0,0 @@
-import random
-
-how_much= 10
-
-questions = ["ら", "は", "さ", "ち", "ほ", "め", "ん", "ま", "む", "ぬ"]
-
-for item in range(0, how_much):
-    ran_questions = random.sample(questions)
-    print(ran_questions)
