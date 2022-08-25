ed227fcc5e4e97b3908d785ceb8816ac066859e1
累了 <100540507+yycx-bjyx@users.noreply.github.com>
2022-08-26 10:17:13 +1200

    statement_decoration

---
+++ 06_statement_decoration_v1.py
@@ -0,0 +1,18 @@
+def statement_generator(statement, decoration):
+
+    sides = decoration * 3
+
+    statement = "{} {} {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
+
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
+
+    return ""
+
+
+statement_generator("Welcome to the Japanese Quiz", "*")
+print()
+statement_generator("CONGRATULATIONS, YOU GOT IT RIGHT", "!")
+print()
